93234cd62f8019d382b0e75f7a06aeb40ff973c1
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2019-02-05 20:15:22 +0100

    Proposal for interaces for handling sinks
    Generic printer interace. Should allow both terminal and line printer
    output

--- src/printer.h
+++ src/printer.h
@@ -17,16 +17,22 @@ struct page;
 typedef void (*printer_feed_bit_t)(struct printer *printer, struct page *page, int x, int y, int bit);
 
 typedef struct printer {
+  char *name;
   printer_feed_bit_t feed_bit;
+  printer_ set_size;
 } printer_t;
 
 typedef struct page {
+  char *name;
   unsigned int width;
   unsigned int height;
   unsigned int dpi;
 } page_t;
 
-void register_printer(printer_t *printer);
-void register_format(printer_t *printer, page_t *page);
+sink_t *printer_register_sink(printer_t *printer);
+void printer_register_page(sink_t *sink, page_t *page);
+sink_t *printer_get_sink(char *name);
+char **printer_get_sink_pages(sink_t *sink);
+void printer_set_sink_page(sink_t *sink, page_t *page);
 
 #endif // PRINTER_H

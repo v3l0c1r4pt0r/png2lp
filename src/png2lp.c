44af81c20d34d950c0e966d263df5b81825e9209
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2019-02-10 17:42:35 +0100

    Implement getopt
    CLI is now ready

--- src/png2lp.c
+++ src/png2lp.c
@@ -1,18 +1,128 @@
 #include <stdio.h>
 #include <string.h>
 #include <png.h>
+#include <getopt.h>
+#include <limits.h>
 
 #include "binrd.h"
 #include "simplepng.h"
 #include "logger.h"
 #include "printer.h"
 
+#define LOG_OPTION (( CHAR_MAX + 1 ))
+
+enum command
+{
+  CMD_PRINT = 0,
+  CMD_LIST,
+  CMD_PAGELIST,
+};
+
 int main(int argc, char **argv)
 {
-  set_numeric_log_level(LEVEL_DEBUG);
+  int c;
+  int digit_optind = 0;
+  struct opts {
+    char *printer;
+    char *page;
+    enum command cmd;
+    char *filename;
+  } opts = {
+    .cmd = CMD_PRINT,
+    .printer = "VT100",
+    .page = "80x25 terminal (no overflow)",
+  };
 
-  char *printer_name = "VT100";
-  char *page_name = "80x25 terminal (no overflow)";
+  while (1)
+  {
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+      /* {name, has_arg, flag, val} */
+      {"printer", required_argument, 0, 'p' },
+      {"page", required_argument, 0, 'P' },
+      {"list", no_argument, 0, 'l' },
+      {"list-pages", required_argument, 0, 'L' },
+      {"log", required_argument, 0, LOG_OPTION },
+      {"help", no_argument, 0, 'h' },
+      {0, 0, 0, 0 }
+    };
+
+    c = getopt_long(argc, argv, "p:P:lL:h",
+        long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 0:
+        /* for default long options
+         * (option->flag==0 -> 4th element of struct option) */
+        printf("option %s", long_options[option_index].name);
+        if (optarg)
+          printf(" with arg %s", optarg);
+        printf("\n");
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+        if (digit_optind != 0 && digit_optind != this_option_optind)
+          printf("digits occur in two different argv-elements.\n");
+        digit_optind = this_option_optind;
+        printf("option %c\n", c);
+        break;
+
+      case 'p':
+        /* --printer=VAL */
+        opts.printer = optarg;
+        break;
+
+      case 'P':
+        /* --page=VAL */
+        opts.page = optarg;
+        break;
+
+      case 'l':
+        /* --list */
+        opts.cmd = CMD_LIST;
+        break;
+
+      case 'L':
+        /* --list-pages=VAL */
+        opts.cmd = CMD_PAGELIST;
+        opts.printer = optarg;
+        break;
+
+      case LOG_OPTION:
+        /* --log=VAL */
+        set_log_level(optarg);
+        break;
+
+      case 'h':
+        /* --printer=VAL */
+        printf("Usage: %s [-p PRINTER] [-P PAGE] FILENAME | -l | -L PRINTER | -h\n",
+            argv[0]);
+        return 0;
+
+      case '?':
+        /* -* not in optstring */
+        break;
+
+      default:
+        /* optstrings not handled (mistakes?) */
+        printf("?? getopt returned character code 0%o ??\n", c);
+    }
+  }
+
+  /* positional arguments */
+  if (optind < argc)
+  {
+    opts.filename = argv[optind++];
+  }
+  if (optind < argc)
+  {
+    ERROR("too many positional arguments (first superfluous: '%s')", argv[optind]);
+  }
 
   int x = 0, y = 0;
   if (argc < 2)
@@ -24,7 +134,7 @@ int main(int argc, char **argv)
   /* init printers */
   register_sinks();
 
-  if (strcmp(argv[1], "-l") == 0)
+  if (opts.cmd == CMD_LIST)
   {
     /* list printers */
     int i;
@@ -37,11 +147,11 @@ int main(int argc, char **argv)
 
     return 0;
   }
-  else if (strcmp(argv[1], "-L") == 0 && argc > 2)
+  else if (opts.cmd == CMD_PAGELIST)
   {
     /* list pages */
     int i;
-    sink_t sink = printer_get_sink(argv[2]);
+    sink_t sink = printer_get_sink(opts.printer);
     char **pages = printer_get_sink_pages(&sink);
 
     for (i = 0; pages[i] != NULL; i++)
@@ -53,8 +163,7 @@ int main(int argc, char **argv)
   }
 
   /* convert png from param 1 */
-  char *fn = argv[1];
-  bitmap_t bmp = simple_png_read(fn);
+  bitmap_t bmp = simple_png_read(opts.filename);
   if (bmp.width == 0 || bmp.height == 0)
   {
     printf("simple_read_png failed\n");
@@ -67,7 +176,7 @@ int main(int argc, char **argv)
   }
 
   /* find requested sink */
-  sink_t sink = printer_get_sink(printer_name);
+  sink_t sink = printer_get_sink(opts.printer);
   if (sink.printer == NULL)
   {
     ERROR("sink not found");
@@ -75,10 +184,10 @@ int main(int argc, char **argv)
   }
 
   /* select page */
-  page_t *page = printer_get_sink_page_by_name(&sink, page_name);
+  page_t *page = printer_get_sink_page_by_name(&sink, opts.page);
   if (page == NULL)
   {
-    ERROR("page '%s' cannot be found for '%s'", page_name, printer_name);
+    ERROR("page '%s' cannot be found for '%s'", opts.page, opts.printer);
     return 1;
   }
   printer_set_sink_page(&sink, page);
